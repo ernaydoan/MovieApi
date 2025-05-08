615f9c5df7388b9a11c8b41b254b3af8d0bfc232
ernaydoan <ern06ayd@gmail.com>
2025-05-09 01:00:44 +0300

    a

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public UpdateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UpdateMovieCommand command)
+        {
+            var value = await _context.Movies.FindAsync(command.MovieId);
+            value.Rating = command.Rating;
+            value.Title = command.Title;
+            value.Description = command.Description;
+            value.Status = command.Status;
+            value.ReleaseDate = command.ReleaseDate;
+            value.CreatedDate = command.CreatedDate;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.Duration = command.Duration;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Presentation/MovieApi.WebApi/Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
+using MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly CreateMovieCommandHandler _createMovieCommandHandler;
+        private readonly GetMovieByIdQueryHandler _getMovieByIdQueryHandler;
+        private readonly RemoveMovieCommandHandler _removeMovieCommandHandler;
+        private readonly UpdateMovieCommandHandler _updateMovieCommandHandler;
+        private readonly GetMovieQueryHandler _getMovieQueryHandler;
+
+        public MoviesController(CreateMovieCommandHandler createMovieCommandHandler, GetMovieByIdQueryHandler getMovieByIdQueryHandler, RemoveMovieCommandHandler removeMovieCommandHandler, UpdateMovieCommandHandler updateMovieCommandHandler, GetMovieQueryHandler getMovieQueryHandler)
+        {
+            _createMovieCommandHandler = createMovieCommandHandler;
+            _getMovieByIdQueryHandler = getMovieByIdQueryHandler;
+            _removeMovieCommandHandler = removeMovieCommandHandler;
+            _updateMovieCommandHandler = updateMovieCommandHandler;
+            _getMovieQueryHandler = getMovieQueryHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MovieList()
+        {
+            var value = await _getMovieQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovie(CreateMovieCommand command)
+        {
+            await _createMovieCommandHandler.Handle(command);
+            return Ok("Movie Ekleme İşlemi Başarılı");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMovie(UpdateMovieCommand command)
+        {
+            await _updateMovieCommandHandler.Handle(command);
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            await _removeMovieCommandHandler.Handle(new RemoveMovieCommand(id));
+            return Ok("Silme işlemi başarılı");
+            
+        }
+
+        [HttpGet("GetMovie")]
+        public async Task<IActionResult> GetMovie(int id)
+        {
+            var value = await _getMovieByIdQueryHandler.Handle(new GetMovieByIdQuery(id));
+            return Ok(value);
+        }
+    }
+
+}
