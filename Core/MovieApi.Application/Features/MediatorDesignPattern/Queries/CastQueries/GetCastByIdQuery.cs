678e6be066a5e1c1580bcd87d7b66f3a0593bf3a
ernaydoan <ern06ayd@gmail.com>
2025-05-13 01:18:51 +0300

    mediator

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/CastQueries/GetCastByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries
+{
+    public class GetCastByIdQuery:IRequest<GetCastByIdQuery>
+    {
+        public int CastId { get; set; }
+
+        public GetCastByIdQuery(int castId)
+        {
+            CastId = castId;
+        }
+    }
+}
