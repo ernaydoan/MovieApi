8f905f321ede1dfc38ef370a3bfc6a4c012a7ffc
ernaydoan <ern06ayd@gmail.com>
2025-05-19 01:04:20 +0300

    UI

---
+++ Frontends/MovieApi.WebUI/Controllers/UserWebUILayoutController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class UserWebUILayoutController1 : Controller
+    {
+        public IActionResult LayoutUI()
+        {
+            return View();
+        }
+    }
+}
