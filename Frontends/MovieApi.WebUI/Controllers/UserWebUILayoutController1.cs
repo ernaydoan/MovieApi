1f8966ae2268b639dcb667b0f5d200927c9aa4dd
ernaydoan <ern06ayd@gmail.com>
2025-05-21 23:11:31 +0300

    UI

--- Frontends/MovieApi.WebUI/Controllers/UserWebUILayoutController1.cs
+++ Frontends/MovieApi.WebUI/Controllers/UserWebUILayoutController1.cs
@@ -2,7 +2,7 @@
 
 namespace MovieApi.WebUI.Controllers
 {
-    public class UserWebUILayoutController1 : Controller
+    public class UserWebUILayoutController : Controller
     {
         public IActionResult LayoutUI()
         {
