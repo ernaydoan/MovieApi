fe521484f6a1ab813b17690f2685282b03c9dc65
ernaydoan <ern06ayd@gmail.com>
2025-05-22 22:58:16 +0300

    UI

---
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MovieList()
+        {
+            ViewBag.v1 = "Movie List";
+            ViewBag.v2 = "Home Page";
+            ViewBag.v3 = "All Movie";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7172/api/Movies");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
